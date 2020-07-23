8f8c9b16a0311972e9d636354b285fd6b3ec0ad7
unknown <laokimsea@gmail.com>
2020-07-23 17:16:37 +0700

    Implement update answer

--- app/Answer.php
+++ app/Answer.php
@@ -8,7 +8,7 @@ use function foo\func;
 class Answer extends Model
 {
     protected $fillable = ['body', 'user_id'];
-    
+
     public function question() {
         return $this->belongsTo(Question::class);
     }
